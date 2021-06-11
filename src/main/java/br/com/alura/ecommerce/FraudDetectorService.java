2d20b10a706c8b96ba67acf0808dcd22581c0488
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-06-11 15:58:14 -0300

    consumer

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,4 +1,52 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class FraudDetectorService {
+    private static final Logger log = LoggerFactory.getLogger(FraudDetectorService.class);
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                log.warn("Foram encontrados {} registros", records.count());
+                for (var record : records) {
+                    log.info("----------------------------------------");
+                    log.info("Processing new order, checking for fraud");
+                    log.info("{}", record.key());
+                    log.info("{}", record.value());
+                    log.info("{}", record.partition());
+                    log.info("{}", record.offset());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    log.info("Order processed");
+                }
+                continue;
+            }
+
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        return properties;
+    }
 }
